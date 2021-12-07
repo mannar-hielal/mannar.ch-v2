5427b341757e8102894505e99aac9700e93423c7
Mannar Hielal <mannar.hielal@liip.ch>
2021-12-08 00:55:46 +0100

    Tweek footer

--- webpack.common.js
+++ webpack.common.js
@@ -4,11 +4,11 @@ const path = require('path');
 var webpack = require('webpack');
 const HtmlWebpackPartialsPlugin = require('html-webpack-partials-plugin');
 
-function generateHtmlPlugins (templateDir) {
+function generateHtmlPlugins(templateDir) {
     // Read files in template directory
     const templateFiles = fs.readdirSync(path.resolve(__dirname, templateDir));
     return templateFiles.map(item => {
-    // Split names and extension
+        // Split names and extension
         const parts = item.split('.');
         const name = parts[0];
         const extension = parts[1];
@@ -31,19 +31,19 @@ module.exports = {
     },
     // define output point
     output: {
-        path: path.resolve(__dirname,'./dist'),
+        path: path.resolve(__dirname, './dist'),
         filename: '[name].[contenthash].bundle.js',
     },
     stats: {
         children: true,
     },
-    module:{
-        rules:[
+    module: {
+        rules: [
             {
                 test: /\.m?js$/,
                 exclude: /(node_modules)/,
                 loader: 'babel-loader',
-                options:{
+                options: {
                     presets: ['@babel/preset-env']
                 }
             },
@@ -79,34 +79,34 @@ module.exports = {
             }
         ]
     },
-    plugins: [new webpack.ProvidePlugin({$: 'jquery', jQuery: 'jquery'}),
-        new HtmlWebpackPartialsPlugin([
-            {
-                path: path.join(__dirname, './app/partials/header.html'),
-                priority: 'high',
-                location: 'body',
-                template_filename: ['01blog-vochabular.html', '404.html','about.html','blog.html','portfolio.html','toplikes.html']
-            }
-        ]),
-        new HtmlWebpackPartialsPlugin([
-            {
-                path: path.join(__dirname, './app/partials/footer.html'),
-                location: 'footer',
-                template_filename: ['01blog-vochabular.html', '404.html','about.html','blog.html','portfolio.html','toplikes.html']
-            }
-        ]),
-        new HtmlWebpackPartialsPlugin([
-            {
-                path: path.join(__dirname, './app/partials/analytics.html'),
-                location: 'analytics',
-                template_filename: ['index.html','01blog-vochabular.html', '404.html','about.html','blog.html','portfolio.html','toplikes.html']
-            }
-        ]),
-        new HtmlWebpackPartialsPlugin([
-            {
-                path: path.join(__dirname, './app/partials/head.html'),
-                location: 'head',
-                template_filename: ['index.html','01blog-vochabular.html', '404.html','about.html','blog.html','portfolio.html','toplikes.html']
-            }
-        ])].concat(htmlPlugins)
+    plugins: [new webpack.ProvidePlugin({ $: 'jquery', jQuery: 'jquery' }),
+    new HtmlWebpackPartialsPlugin([
+        {
+            path: path.join(__dirname, './app/partials/header.html'),
+            priority: 'high',
+            location: 'body',
+            template_filename: ['01blog-vochabular.html', '404.html', 'about.html', 'blog.html', 'portfolio.html', 'toplikes.html']
+        }
+    ]),
+    new HtmlWebpackPartialsPlugin([
+        {
+            path: path.join(__dirname, './app/partials/footer.html'),
+            location: 'footer',
+            template_filename: ['index.html', '01blog-vochabular.html', '404.html', 'about.html', 'blog.html', 'portfolio.html', 'toplikes.html']
+        }
+    ]),
+    new HtmlWebpackPartialsPlugin([
+        {
+            path: path.join(__dirname, './app/partials/analytics.html'),
+            location: 'analytics',
+            template_filename: ['index.html', '01blog-vochabular.html', '404.html', 'about.html', 'blog.html', 'portfolio.html', 'toplikes.html']
+        }
+    ]),
+    new HtmlWebpackPartialsPlugin([
+        {
+            path: path.join(__dirname, './app/partials/head.html'),
+            location: 'head',
+            template_filename: ['index.html', '01blog-vochabular.html', '404.html', 'about.html', 'blog.html', 'portfolio.html', 'toplikes.html']
+        }
+    ])].concat(htmlPlugins)
 };
